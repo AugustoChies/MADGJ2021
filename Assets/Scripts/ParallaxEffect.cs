d4f1a08d4ab02ad8cafb8ce4913a42856380fefa
Gabriel Pittol <gabrielpittol@hotmail.com>
2021-02-26 21:16:58 -0300

    parallax

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    private Transform _cameraTransform;
+    private Vector3 _lastCameraPos;
+    [SerializeField] private Vector2 effectMultiplier;
+
+    void Start()
+    {
+        _cameraTransform = Camera.main.transform;
+        _lastCameraPos = _cameraTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = _cameraTransform.position - _lastCameraPos;
+        transform.position += new Vector3(deltaMovement.x * effectMultiplier.x, deltaMovement.y * effectMultiplier.y, deltaMovement.z) ;
+        _lastCameraPos = _cameraTransform.position;
+    }
+}
